a2a5e170e341de12f74757485ae25dc2eaeb3828
mpaulofg <mpaulofg@gmail.com>
2022-07-13 08:34:20 -0300

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,6 +9,9 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -16,7 +19,7 @@ public class Main extends Application {
 			ScrollPane scrollpane = loader.load();
 			scrollpane.setFitToHeight(true);
 			scrollpane.setFitToWidth(true);
-			Scene mainScene = new Scene(scrollpane);
+			mainScene = new Scene(scrollpane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
@@ -25,6 +28,10 @@ public class Main extends Application {
 		}
 	}
 
+	public static Scene getMainScene() {
+		return mainScene;
+	}
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
